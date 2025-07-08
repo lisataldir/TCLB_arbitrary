dd1968520f65264c97bea07267057e70d3c3479f
lisataldir <lisa.taldir@free.fr>
2025-07-08 03:32:27 +0200

    [08/07] Remove useless functions

--- src/Handlers/acArbitraryLattice.cpp
+++ src/Handlers/acArbitraryLattice.cpp
@@ -4,7 +4,6 @@ std::string acArbitraryLattice::xmlname = "ArbitraryLattice";
 
 int acArbitraryLattice::Init() {
     const auto lattice = solver->getArbLattice();
-    lattice->CutsOverwrite(lattice->connect.cuts.get());
     return lattice->reinitialize(solver->num_snaps, solver->setting_zones, node);
 
 }
